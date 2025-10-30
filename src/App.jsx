d7b2f66710892191be2e1e647ab7295deceee2bc
kuu3114as-beep <kuu.3114.as@gmail.com>
2025-10-30 23:32:48 +0900

    Create initial App component with welcome message

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+mport React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🌸 Tsubomiへようこそ 🌸</h1>
+      <p>ここにあなたのAIチャットアプリが咲きます。</p>
+    </div>
+  );
+}
+
+export default App;
